d8eb335b3de2de373eed552de24735bc9950c24e
ajaymiryala2002 <ajaymiryala259@gmail.com>
2025-11-28 22:58:22 +0530

    render setup

--- start.sh
+++ start.sh
@@ -6,11 +6,7 @@
 
 #!/bin/bash
 
-# Collect static files
+#!/bin/bash
 python manage.py collectstatic --noinput
-
-# Apply migrations
 python manage.py migrate --noinput
-
-# Start Gunicorn
-gunicorn your_project_name.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn trinity_clg.wsgi:application --bind 0.0.0.0:$PORT
